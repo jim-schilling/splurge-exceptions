7b860e6460965299cfd1185e2ed87de534cbec48
Jim Schilling <jimschilling@outlook.com>
2025-10-25 20:36:19 -0500

    Remove empty exception handling modules: delete error_handler.py and exception.py as they no longer serve a purpose.

--- splurge_exceptions/decorators/error_handler.py
+++
@@ -1,5 +0,0 @@
-"""Exception handling decorators module - currently empty.
-
-This module previously contained handle_exceptions() decorator which has been removed
-in favor of direct try/except exception handling with low-level exceptions.
-"""
--- splurge_exceptions/managers/exception.py
+++
@@ -1,5 +0,0 @@
-"""Exception managers module - currently empty.
-
-This module previously contained error_context() which has been removed
-in favor of simpler patterns: @handle_exceptions() decorator or low-level try/except.
-"""
